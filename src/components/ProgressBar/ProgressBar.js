59e676af8087ddbf4ae55954ac897019d63dd625
Antony Sande <sandeantony@gmail.com>
2021-04-20 06:48:05 +0300

    feat: add progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import styled from 'styled-components';
 
@@ -6,7 +5,51 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  let height;
+
+  if (size === 'small') {
+    height = 8;
+  } else if (size === 'medium') {
+    height = 12;
+  } else if (size === 'large') {
+    height = 24;
+  } else {
+    throw new Error(`Unrecognized ProgressBar size: ${size}`);
+  }
+
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{ '--padding': size === 'large' ? '4px' : 0 }}
+      size={size}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarWrapper>
+        <Bar style={{ '--width': value + '%', '--height': height + 'px' }} />
+      </BarWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 4px;
+  padding: var(--padding);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
